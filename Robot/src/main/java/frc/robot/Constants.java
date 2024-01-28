8e6a9660df8bb83094b9e78107c847824a0e6742
Impxrtant322 <63988752+Impxrtant322@users.noreply.github.com>
2024-01-27 18:02:18 -0700

    fixed up settings

--- Robot/src/main/java/frc/robot/Constants.java
+++ Robot/src/main/java/frc/robot/Constants.java
@@ -22,6 +22,7 @@ public final class Constants {
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
   public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
     public static final double LEFT_X_DEADBAND  = 0.1;
@@ -29,4 +30,36 @@ public final class Constants {
     public static final double RIGHT_X_DEADBAND = 0.1;
     public static final double TURN_CONSTANT    = 6;
   }
+  
+  public final class Controller {
+
+		public static final int m_controller = 0;
+
+		public final class Joystick {
+
+			public static final int m_leftStickY = 1;
+			public static final int m_leftStickX = 0;
+			public static final int m_rightStickX = 4;
+			// public static final int m_rightTrigger = 3;
+
+
+
+			public static final int m_leftTrigger = 2;
+			public static final int m_rightTrigger = 3;
+		}
+
+		public final class Buttons {
+
+			public static final int m_aButton = 1;
+			public static final int m_bButton = 2;
+			public static final int m_xButton = 3;
+			public static final int m_yButton = 4;
+		}
+
+		public final class Bumpers {
+
+			public static final int m_lBumper = 5;
+			public static final int m_rBumper = 6;
+		}
+	}
 }
--- Robot/src/main/java/frc/robot/RobotContainer.java
+++ Robot/src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,10 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.drivebase.AbsoluteDriveAdv;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 import java.io.File;
@@ -36,6 +39,20 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
     private final XboxController driverXbox = new XboxController(OperatorConstants.kDriverControllerPort);
+
+	public Trigger xButtonDriver = new JoystickButton(driverXbox, Constants.Controller.Buttons.m_xButton);
+	public Trigger yButtonDriver = new JoystickButton(driverXbox, Constants.Controller.Buttons.m_yButton);
+	public Trigger bButtonDriver = new JoystickButton(driverXbox, Constants.Controller.Buttons.m_bButton);
+	public Trigger aButtonDriver = new JoystickButton(driverXbox, Constants.Controller.Buttons.m_aButton);
+
+	public Trigger rBumperDriver = new JoystickButton(driverXbox, Constants.Controller.Bumpers.m_rBumper);
+	public Trigger lBumperDriver = new JoystickButton(driverXbox, Constants.Controller.Bumpers.m_lBumper);
+
+	public POVButton UPDriver = new POVButton(driverXbox, 0);
+	public POVButton DOWNDriver = new POVButton(driverXbox, 180);
+	public POVButton LEFTDriver = new POVButton(driverXbox, 270);
+	public POVButton RIGHTDriver = new POVButton(driverXbox, 90);
+
     SwerveSubsystem drivebase = new SwerveSubsystem();
   // Replace with CommandPS4Controller or CommandJoystick if needed
 
@@ -79,6 +96,8 @@ public class RobotContainer {
 
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+
+    xButtonDriver.onTrue(new InstantCommand(drivebase::zeroGyro));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
